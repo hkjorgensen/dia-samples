a81ec76f8471183dad59ed51c41b8018b4f85755
Clint Heyer <clint@thestaticvoid.net>
2014-02-18 18:37:12 +0100

    Tweaks - touch-playground
    Simplify and use jQuery-hammer

--- touch-playground/script.js
+++ touch-playground/script.js
@@ -1,38 +1,41 @@
-/*
- *
- * Setup
- *
-*/
 
-//Prevent normal iOS/Android touch gestures
-$('body').on('touchmove', function(e) { e.preventDefault() });
+$(document).ready(function() {
+	//Prevent normal iOS/Android touch gestures
+	$('body').on('touchmove', function(e) { e.preventDefault() });
 
-//Enable extra debug on desktop browsers
-Hammer.plugins.showTouches();
-Hammer.plugins.fakeMultitouch();
+  // Initialise Hammer (important!)
+  $('body').hammer({prevent_default:true});
 
-/*
- *
- * Custom actions
- *
-*/
+	//Enable extra debug on desktop browsers
+	Hammer.plugins.showTouches();
+	Hammer.plugins.fakeMultitouch();
 
-//Create a reference to the element
-var element = document.querySelector('section');
-
-//What kind of events should be logged
-var events = [
-  'touch', 'release', 'hold', 'tap', 'doubletap',
-  'drag', 'dragstart', 'dragend', 'dragleft', 'dragright', 'dragup', 'dragdown',
-  'swipe', 'swipeleft', 'swiperight', 'swipeup', 'swipedown',
-  'transformstart', 'transform', 'transformend',
-  'rotate', 'rotateleft', 'rotateright',
-  'pinch', 'pinchin', 'pinchout'
-];
-
-//Change the color of the element when it is touched
-Hammer(element).on(events.join(' '), function(e) {
-  var name = '<div>'+ e.type +'</div>';
-
-  $('aside strong').after(name);
+	//What kind of events should be logged
+	// 'events.join' later on 
+	var events = [
+	  'touch', 'release', 'hold', 'tap', 'doubletap',
+	  'drag', 'dragstart', 'dragend', 'dragleft', 'dragright', 'dragup', 'dragdown',
+	  'swipe', 'swipeleft', 'swiperight', 'swipeup', 'swipedown',
+	  'transformstart', 'transform', 'transformend',
+	  'rotate', 'rotateleft', 'rotateright',
+	  'pinch', 'pinchin', 'pinchout'
+	];
+	
+	// This converts the array above to one long string with each name separated by a space
+	// eg: "touch release hold tap ..."
+	var eventNames = events.join(' ');
+	$('section').on(eventNames, onEvent);
 });
+
+
+function onEvent(e) {
+	// e.originalEvent contains a lot of the useful data
+  var g = e.gesture;
+  var data = '<div>' +
+  					 '<div><strong>'+ e.type + '</strong></div>' +
+  					 '<div>angle: ' + g.angle + '  direction: ' + g.direction + '</div>' +
+  					 '<div>velocity: ' + g.velocityX + ', ' + g.velocityY + '</div>' +
+
+  					 '</div>'
+  $('aside').prepend(data);
+}
