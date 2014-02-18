bcd43b63f34bf70a86d951a189c4a0b111311544
Clint Heyer <clint@thestaticvoid.net>
2014-02-18 18:39:26 +0100

    Tweaks - pointer-playground
    Simplified

--- pointer-playground/script.js
+++ pointer-playground/script.js
@@ -7,23 +7,17 @@
 //Prevent normal iOS/Android touch gestures
 $('body').on('touchmove', function(e) { e.preventDefault() });
 
-/*
- *
- * Custom actions
- *
-*/
+// Listen to all of the pointer events within the 'section' element
+$('section').on("pointerdown pointerup pointermove pointerover pointerout pointerenter pointerleave", function(e) {
+	// Print out some useful information to a DIV
+  
+	// e.originalEvent contains a lot of the useful data
+  var orig = e.originalEvent;
 
-var events = [
-  'pointerdown',
-  'pointerup',
-  'pointermove',
-  'pointerover',
-  'pointerout',
-  'pointerenter',
-  'pointerleave'
-];
 
-$('section').on(events.join(' '), function(e) {
-  var name = '<div>'+e.type+'</div>';
-  $('aside').prepend(name);
+  var data = '<div>' +
+  					 '<div><strong>'+ e.type + '</strong></div>' +
+  					 '<div>' + orig.pointerType + ' x: ' + Math.floor(orig.x) + " y: " + Math.floor(orig.y) + " button: " + orig.button + " id: " +  orig.pointerId + '</div>' +
+  					 '</div>'
+  $('aside').prepend(data);
 });
