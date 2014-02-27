1ab3cd69b593f882e2dabf6bcd3f0311a29453be
Clint Heyer <clint@thestaticvoid.net>
2014-02-27 12:45:53 +0100

    Updated pointer-drag to show how to detect intersecting elements

--- pointer-drag/script.js
+++ pointer-drag/script.js
@@ -1,25 +1,43 @@
-/*
- *
- * Setup
- *
-*/
 
-//Prevent normal iOS/Android touch gestures
-$('body').on('touchmove', function(e) { e.preventDefault() });
+$(document).ready(function() {
+	// Prevent normal iOS/Android touch gestures
+	$('body').on('touchmove', function(e) { e.preventDefault() });
+	
+	// Wire up event
+	$('body').on('pointermove', onPointerMove);
 
-/*
- *
- * Custom actions
- *
-*/
-
-$('body').on('pointermove', function(e) {
-  var top = e.originalEvent.clientY;
-  var left = e.originalEvent.clientX;
-  var coordinates = '<div>X: '+ left +'</div><div>Y: '+ top +'</div>';
-
-  $('section')
-    .css('left', left)
-    .css('top', top)
-    .html(coordinates);
 });
+
+function onPointerMove(e) {
+  var pointerTop = e.originalEvent.clientY;
+  var pointerLeft = e.originalEvent.clientX;
+  var coordinates = '<div>X: '+ pointerLeft +'</div><div>Y: '+ pointerTop +'</div>';
+
+  // Get the size of element with id 'dragBox'
+  var size = $("#dragBox").size();
+
+  // Move the element with id 'dragBox' to follow the cursor,
+  // with the cursor in the middle
+  $('#dragBox')
+    .css({
+    	left: pointerLeft - (size.width/2.0),
+    	top: pointerTop - (size.height/2.0)
+    })
+    .html(coordinates);
+
+  // Gets a list of elements with class 'targetBox' that intersect
+  // with an element with id 'dragBox'
+  var intersecting = $(".targetBox").findIntersecting("#dragBox");
+  // 		Tip: you can also use 'findNotIntersecting' to get a list of those elements that DON'T intersect
+
+  // set some text for those that intersect
+  intersecting.text("I've been hit");
+
+  // 'intersects' simply returns true or false if anything with class 'targetBox'
+  // intersects with element of id 'dragBox'
+  var intersects = $('.targetBox').intersects('#dragBox');
+  if (intersects)
+  	$("#dragBox").css("background-color", "red");
+  else
+  	$("#dragBox").css("background-color", ""); // Un-set colour so it goes back to the default
+}
