1c5ac343ca8698ee7f1048a082ee71675b581d43
Henrik Kok <kok@me.com>
2014-03-28 00:55:47 +0100

    added motion-sound demo

---
+++ motion-sound/README.md
@@ -0,0 +1,7 @@
+# motion-sound
+
+Read more:
+Add samsung TV movie
+https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
+http://www.html5rocks.com/en/tutorials/device/orientation/
+
---
+++ motion-sound/script.js
@@ -0,0 +1,91 @@
+// Initialise a few variables
+var socket = null;
+var audio = null;
+var gain = null;
+var oscillator = null;
+var volume = null;
+
+//When the browser is ready
+$(document).ready(function() {
+  // Connect realtime stuff up
+  socket = io.connect('/');
+  socket.on('say', onSay);
+
+  //Setup sound modules
+  audio = kattegatAudio.initialize();
+  oscillator = audio.createOscillator();
+  gain = audio.createGain();
+
+  //The Gain connects to the destination (speakers)
+  gain.connect(audio.destination);
+
+  //Set the volume to zero
+  volume = 0;
+  gain.gain.value = volume;
+
+  //Setup the frequency
+  oscillator.type = oscillator.TRIANGLE;
+
+  //The oscillator connects to the Gain
+  oscillator.connect(gain);
+
+  //Start the oscillator immediatly
+  oscillator.start(0);
+
+  //Attach eventlisteners to window
+  $(window).on('devicemotion', onDeviceMotion);
+});
+
+//Collect data and send it to the server
+function onDeviceMotion(e) {
+  var motion = e.originalEvent;
+
+  // console.log(motion);
+  var acceleration = {
+    x: motion.acceleration.x,
+    y: motion.acceleration.y,
+    z: motion.acceleration.z
+  };
+  var accelerationIncludingGravity = {
+    x: motion.accelerationIncludingGravity.x,
+    y: motion.accelerationIncludingGravity.y,
+    z: motion.accelerationIncludingGravity.z
+  };
+  var rotationRate = {
+    alpha: motion.rotationRate.alpha,
+    beta: motion.rotationRate.beta,
+    gamma: motion.rotationRate.gamma
+  };
+
+  socket.emit('say', {
+    acceleration: acceleration,
+    accelerationIncludingGravity: accelerationIncludingGravity,
+    rotationRate: rotationRate
+  });
+}
+
+//Do something with the data from a third device (Phone, tablet etc.)
+function onSay(motion) {
+  updateSound(motion);
+}
+
+function updateSound(motion) {
+  var z = motion.accelerationIncludingGravity.z;
+
+  //Make it an integer: 1.111 = 1
+  z = Math.round(z);
+
+  // make z less strong 1 = 0.002
+  z = z / 500;
+
+  //Apply data to the current volume
+  volume = volume + z;
+
+  //Keep sound with boundaries 1/0 (max/min)
+  volume = Math.max(0, volume);
+  volume = Math.min(1, volume);
+
+  //Apply new values
+  gain.gain.value = volume;
+  $('#volume').html(Math.round( volume*100 ));
+}
