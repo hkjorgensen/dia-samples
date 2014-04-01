e1e9edd2cdc9c67ae01dadc81d2a65bc21914441
Clint Heyer <clint@thestaticvoid.net>
2014-04-01 13:13:52 +0200

    Updated 'motion-force'

--- motion-force/script.js
+++ motion-force/script.js
@@ -1,41 +1,23 @@
 // Initialise a few variables
 var socket = null;
-//Small function to check if phone device
-var isMobile = {
-  Android: function() {
-      return navigator.userAgent.match(/Android/i);
-  },
-  iOS: function() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function() {
-      return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function() {
-      return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function() {
-      return (isMobile.Android() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-  }
-};
 
-//When the browser is ready
+// When the browser is ready
 $(document).ready(function() {
   // Connect realtime stuff up
   socket = io.connect('/');
   socket.on('say', onSay);
 
-  //Trigger is mobile show overlay
-  if ( isMobile.any() ) {
+  // Trigger is mobile show overlay
+  if (kattegat.device.mobile()) {
     $('aside').hide();
     $('#overlay').show();
   } else {
-    //Attach eventlisteners to window
+    // Attach eventlisteners to window
     $(window).on('devicemotion', onDeviceMotion);
   }
 });
 
-//Collect data and send it to the server
+// Collect data and send it to the server
 function onDeviceMotion(e) {
   var motion = e.originalEvent;
 
@@ -63,30 +45,34 @@ function onDeviceMotion(e) {
   });
 }
 
-//Do something with the data from a third device (Phone, tablet etc.)
+// Do something with the data from a third device (Phone, tablet etc.)
 function onSay(motion) {
   updateBoxPosition(motion);
 }
 
 function updateBoxPosition(motion) {
+  var d = motion.accelerationIncludingGravity;
+
+  // Get sizes of window and box
   var screenSize = $(window).size();
   var boxSize = $('aside').size();
-  //Use the X & Y acc. to change the placement
+
+  // Get the current left and top values
   var left = $('aside').offset().left;
   var top = $('aside').offset().top;
 
-  //Apply accelration
-  left = left + motion.accelerationIncludingGravity.x;
-  top = top + motion.accelerationIncludingGravity.y;
+  // Apply accelration
+  left = left + d.x;
+  top = top + d.y;
 
-  //Test that the box does not leave the screen
+  // Make sure the box stays within the window
   top = Math.max(top, 0);
   left = Math.max(left, 0);
 
   top = Math.min(top, (screenSize.height - boxSize.height));
   left = Math.min(left, (screenSize.width - boxSize.width));
 
-  //Apply the new values
+  // Apply the new values
   $('aside').css({
     top: top,
     left: left
