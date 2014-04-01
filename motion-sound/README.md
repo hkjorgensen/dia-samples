3f28b3e40a9d8be89e506b1c65a4ab1473900ff0
Clint Heyer <clint@thestaticvoid.net>
2014-04-01 13:26:42 +0200

    Cleaned up 'motion-sound'

--- motion-sound/README.md
+++ motion-sound/README.md
@@ -1,7 +1,6 @@
 # motion-sound
 
 Read more:
-Add samsung TV movie
-https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
-http://www.html5rocks.com/en/tutorials/device/orientation/
+* [Orientation and Motion Data Explained](https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained)
+* [This End Up: Using Device Orientation](http://www.html5rocks.com/en/tutorials/device/orientation/)
 
--- motion-sound/script.js
+++ motion-sound/script.js
@@ -5,93 +5,90 @@ var gain = null;
 var oscillator = null;
 var volume = null;
 
-//When the browser is ready
+// When the browser is ready
 $(document).ready(function() {
   // Connect realtime stuff up
   socket = io.connect('/');
-  socket.on('say', onSay);
 
-  //Setup sound modules
-  audio = kattegatAudio.initialize();
-  oscillator = audio.createOscillator();
-  gain = audio.createGain();
-
-  //The Gain connects to the destination (speakers)
-  gain.connect(audio.destination);
-
-  //Set the volume to zero
-  volume = 0;
-  gain.gain.value = volume;
-
-  //Setup the frequency
-  oscillator.type = oscillator.TRIANGLE;
-
-  //The oscillator connects to the Gain
-  oscillator.connect(gain);
-
-  //Start the oscillator immediatly
-  oscillator.start(0);
-
-  //Trigger is mobile show overlay
-  if ( isMobile.any() ) {
+  if (kattegat.device.mobile() ) {
+    // Mobile device, don't play here
     $('p').hide();
     $('#overlay').show();
   } else {
-    //Attach eventlisteners to window
+    // Not a mobile
+    initAudio();
+    socket.on('say', onSay);  
+  }
+
+  if (window.DeviceMotionEvent) {
     $(window).on('devicemotion', onDeviceMotion);
   }
+   
 });
 
-//Collect data and send it to the server
+function initAudio() {
+  audio = kattegatAudio.initialize();
+  if (audio == null) {
+    kattegat.notify("Web audio not supported");
+    return;
+  }
+
+  oscillator = audio.createOscillator();
+  gain = audio.createGain();
+
+  // The Gain connects to the destination (speakers)
+  gain.connect(audio.destination);
+
+  // Set the volume to zero
+  volume = 0;
+  gain.gain.value = volume;
+
+  // Setup the frequency
+  oscillator.type = oscillator.TRIANGLE;
+
+  // The oscillator connects to the Gain
+  oscillator.connect(gain);
+
+  // Start the oscillator immediatly
+  oscillator.start(0);
+}
+
+// Collect data and send it to the server
 function onDeviceMotion(e) {
   var motion = e.originalEvent;
-
-  // console.log(motion);
-  var acceleration = {
-    x: motion.acceleration.x,
-    y: motion.acceleration.y,
-    z: motion.acceleration.z
-  };
   var accelerationIncludingGravity = {
     x: motion.accelerationIncludingGravity.x,
     y: motion.accelerationIncludingGravity.y,
     z: motion.accelerationIncludingGravity.z
   };
-  var rotationRate = {
-    alpha: motion.rotationRate.alpha,
-    beta: motion.rotationRate.beta,
-    gamma: motion.rotationRate.gamma
-  };
 
-  socket.emit('say', {
-    acceleration: acceleration,
-    accelerationIncludingGravity: accelerationIncludingGravity,
-    rotationRate: rotationRate
-  });
+  socket.emit('say', accelerationIncludingGravity);
 }
 
-//Do something with the data from a third device (Phone, tablet etc.)
+// Do something with the data from a third device (Phone, tablet etc.)
 function onSay(motion) {
   updateSound(motion);
 }
 
 function updateSound(motion) {
-  var z = motion.accelerationIncludingGravity.z;
+  var z = motion.z;
 
-  //Make it an integer: 1.111 = 1
+  // Make it an integer: 1.111 = 1
   z = Math.round(z);
 
-  // make z less strong 1 = 0.002
+  // Make z less strong 1 = 0.002
   z = z / 500;
 
-  //Apply data to the current volume
+  // Apply data to the current volume
   volume = volume + z;
 
-  //Keep sound with boundaries 1/0 (max/min)
+  // Keep sound with boundaries 1/0 (max/min)
   volume = Math.max(0, volume);
   volume = Math.min(1, volume);
 
-  //Apply new values
-  gain.gain.value = volume;
+  // Apply new values if web audio was initalised
+  if (gain) {
+    gain.gain.value = volume;
+  }
   $('#volume').html(Math.round( volume*100 ));
 }
